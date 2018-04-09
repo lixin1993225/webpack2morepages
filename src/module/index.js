837ad6379243a55af127332c7429aad7e31905d0
lizn <lixin_webcode@163.com>
2018-04-09 23:55:34 +0800

    aa

---
+++ src/module/index.js
@@ -0,0 +1,2 @@
+console.log(123)
+require('../assets/index.styl')
--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,138 @@
-const path = require("path");
-const webpack = require("webpack");
-const glob = require("glob");
-const HTMLPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');//代码分离
-
-let entriesObj = getView('./src/js/*.js');
-let pages = Object.keys(getView('./src/*html'));
-
-let config = {
-    entry:entriesObj,
-    output:{
-        path:path.join(__dirname,'./dist'),
-        filename:'[name]/[name].min.js',
-        chunkFilename:'[name].chunk.js'
+var path = require('path');
+var webpack = require('webpack');
+var glob = require('glob');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var CleanPlugin = require('clean-webpack-plugin');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+function getView(globPasth){
+    let files = glob.sync(globPasth);
+    let entries = {},
+        entry,
+        dirname,
+        basename,
+        pathname,
+        extname;
+    files.forEach(item=>{
+        entry = item;
+        dirname = path.dirname(entry);//当前目录,不包含文件
+        extname = path.extname(entry);//文件后缀
+        basename = path.basename(entry,extname);//文件得名字，不带后缀得文件名字
+        pathname = path.join(dirname,basename);//文件路径
+        if(extname ==='.html'){
+            entries[pathname] = './'+ entry;
+        }else{
+            entries[basename] = entry
+        }
+    });
+    return entries
+};
+let entriesObj = getView('./src/module/*.js');
+var prod = process.env.NODE_ENV === 'production' ? true : false;
+var config = {
+    entry: entriesObj,
+    output: {
+        path: path.resolve(__dirname, prod ? "./dist" : "./build"),
+        filename: prod ? "module/[name].min.js" : "module/[name].js",
+        chunkFilename: 'module/[name].chunk.js',
+        publicPath: prod ? "http:cdn.mydomain.com" : ""
     },
-    module:{
-        rules:[
+    resolve: {
+        //配置项,设置忽略js后缀
+        extensions: ['', '.js', '.less', '.css', '.png', '.jpg'],
+        root: './src',
+        // 模块别名
+        alias: {}
+    },
+    module: {
+        loaders: [
             {
-                test:/\.css$/,
-                use:[
-                    'style-loader',
-                    'css-loader'
-                ]
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'url?limit=10000&name=images/[name].[ext]'
+            }, 
+            {
+                test: /\.styl$/,
+                loader: ExtractTextPlugin.extract('style', 'css!stylus')
             },
             {
-                test:/\.(gif|png|jpeg|jpg)$/,
-                use:[
-                    {
-                        loader:'url-loader',
-                        options:{
-                            limit:1024,
-                            name:'[name]-lizn.[ext]'
-                        }
-                    }
-                ]
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel?presets[]=es2015&presets[]=react'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html?attrs=img:src img:srcset'
             }
-
         ]
     },
-    plugins:[
-        new ExtractTextPlugin('[name].css',{
-            allChunks:true
-        })
+    plugins: [
+        new CleanPlugin(['dist', 'build']),
+        // 启动热替换
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('assets/[name].css', {
+            allChunks: true
+        }),
+        new webpack.NoErrorsPlugin(),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:8080'
+        }),
+        /* 公共库 */
+        new CommonsChunkPlugin({
+            name: 'vendors',
+            minChunks: Infinity
+        }),
     ]
 };
 
-function getView(globPath, pathDir) {//entry入口文件配置
-    var files = glob.sync(globPath);
-    var entries = {},
-        entry, 
-        dirname, 
-        basename, 
-        pathname, 
-        extname;
-        files.forEach(item=>{
-            entry = item;
-            dirname = path.dirname(entry);//当前目录
-            extname = path.extname(entry);//输出文件名后缀
-            basename = path.basename(entry,extname);//输出的是不含后缀的文件名
-            pathname = path.join(dirname,basename);//文件路径
-            if(extname === '.html'){
-                entries[pathname] = './' + entry
-            }else{
-                entries[basename] = entry
-            }
-        })
-    return entries;
-};
-
+let pages = Object.keys(getView('./src/*/*.html'));//获取页面
 pages.forEach(pathname=>{
     let htmlname = pathname.split('src\\')[1];
+    let extname2 = path.extname(pathname);//文件后缀
+    let basename = path.basename(pathname,extname2)
     let conf = {
         filename:`${htmlname}.html`,
         template:`${pathname}.html`,
         hash:true,
-        chunks:[htmlname],
-        minify:{
-            removeAttributeQuotes:true,
-            removeComments: true,
-            collapseWhitespace: true,
-            removeScriptTypeAttributes:true,
-            removeStyleLinkTypeAttributes:true
-        }
+        chunks:[basename],
+        // minify:{
+        //     removeAttributeQuotes:true,
+        //     removeComments: true,
+        //     collapseWhitespace: true,
+        //     removeScriptTypeAttributes:true,
+        //     removeStyleLinkTypeAttributes:true
+        // }
     }
-    config.plugins.push(new HTMLPlugin(conf))
-});
-
-
-config.devServer = {
-    port : 8080,
-    host : '0,0,0,0',
-    overlay:{
-        errors:true,
-    },
-    hot:true
-};
-config.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-)
+    config.plugins.push(new HtmlWebpackPlugin(conf))
+})
+// 判断开发环境还是生产环境,添加uglify等插件
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = (module.exports.plugins || [])
+        .concat([
+            new webpack.DefinePlugin({
+                __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.optimize.OccurenceOrderPlugin(),
+        ]);
+} else {
+    module.exports.devtool = 'source-map';
+    module.exports.devServer = {
+        port: 8080,
+        contentBase: './build',
+        hot: true,
+        historyApiFallback: true,
+        publicPath: "",
+        stats: {
+            colors: true
+        },
+        plugins: [
+        new webpack.HotModuleReplacementPlugin()
+        ]
+    };
+}
 module.exports = config
